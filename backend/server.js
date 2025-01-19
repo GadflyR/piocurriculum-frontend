224c02402939aa59415220de9ccbba7d3660b83f
GadflyR <ottomanhabsburg@gmail.com>
2025-01-19 12:24:58 -0500

    1

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,11 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 // ===================== 1) STATIC CONSTANTS (like Java) =====================
 const MAX_PLANS = 1000;
 
